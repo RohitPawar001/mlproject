00219988577a6a7b9d9516220a2ad1fcc567048a
RohitPawar001 <rppawar491@gmail.com>
2024-09-27 13:39:48 +0530

    eda

--- src/Exceptions.py
+++ src/Exceptions.py
@@ -25,5 +25,5 @@ if __name__ == "__main__":
         a = 1/0
         
     except Exception as e:
-        logging.info("Logging has started")
+        logging.info("Loggin")
         raise CustomException(e,sys)
