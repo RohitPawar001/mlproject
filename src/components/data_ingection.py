a2f2abbcd5611a9363e194cef8f249384ff8dfa4
RohitPawar001 <rppawar491@gmail.com>
2024-09-28 17:14:39 +0530

    model trainer

--- src/components/data_ingection.py
+++ src/components/data_ingection.py
@@ -10,8 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-#from src.components.model_trainer import ModelTrainerConfig
-#from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -56,7 +57,7 @@ if __name__=="__main__":
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    #modeltrainer=ModelTrainer()
-    #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_training(train_arr,test_arr))
 
 
--- src/utils.py
+++ src/utils.py
@@ -22,3 +22,27 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
+def evaluate_models(x_train,y_train,x_test,y_test,models):
+    try:
+        report = {}
+        
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(x_train,y_train)
+            
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+            
+            train_model_score = r2_score(y_train_pred,y_train)
+            test_model_score = r2_score(y_test_pred,y_test)
+            
+            report[list(models.keys())[i]] = test_model_score
+            
+            return report
+        
+    except Exception as e:
+        raise CustomException(e,sys)
+            
+            
+    
+    
